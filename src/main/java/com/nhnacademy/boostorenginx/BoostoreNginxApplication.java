4f37109de538501df9af670031ee1f645d2709e7
jalyice <jalyice@gmail.com>
2024-12-10 15:31:41 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/boostorenginx/BoostoreNginxApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.boostorenginx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostoreNginxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoostoreNginxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/boostorenginx/BoostoreNginxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.boostorenginx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoostoreNginxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
