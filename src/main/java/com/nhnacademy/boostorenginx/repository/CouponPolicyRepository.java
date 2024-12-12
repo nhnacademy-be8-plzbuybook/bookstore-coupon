f90c906a5f0da7e586e69d28a6436d011562d63f
kkkkimtaehyeon <kth1927@naver.com>
2024-12-12 17:31:23 +0900

    쿠폰정책 등록 구현

---
+++ src/main/java/com/nhnacademy/boostorenginx/repository/CouponPolicyRepository.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.boostorenginx.repository;
+
+import com.nhnacademy.boostorenginx.entity.CouponPolicy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponPolicyRepository extends JpaRepository<CouponPolicy, Long> {
+}
---
+++ src/main/java/com/nhnacademy/boostorenginx/repository/CouponTargetRepository.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.boostorenginx.repository;
+
+import com.nhnacademy.boostorenginx.entity.CouponTarget;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponTargetRepository extends JpaRepository<CouponTarget, Long> {
+}
