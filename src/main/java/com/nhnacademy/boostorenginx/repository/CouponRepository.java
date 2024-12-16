7c695a6d10d4d283991cf2b8fb9310c0863ba443
thr13 <skhrnt2945@gmail.com>
2024-12-16 14:04:10 +0900

    쿠폰 Repository 수정

--- src/main/java/com/nhnacademy/boostorenginx/repository/CouponRepository.java
+++ src/main/java/com/nhnacademy/boostorenginx/repository/CouponRepository.java
@@ -1,11 +1,29 @@
 package com.nhnacademy.boostorenginx.repository;
 
 import com.nhnacademy.boostorenginx.entity.Coupon;
+import com.nhnacademy.boostorenginx.entity.CouponPolicy;
+import com.nhnacademy.boostorenginx.enums.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
-    Optional<Coupon> findByCode(String code); // 쿠폰 코드로 쿠폰 객체 조회
+    Optional<Coupon> findByCode(String code); // 쿠폰코드로 쿠폰 객체 조회
 
+    List<Coupon> findByStatus(Status status); // 쿠폰상태로 쿠폰들 조회
+
+    List<Coupon> findByExpiredAtBefore(LocalDateTime currentDateTime); // 만료된 쿠폰들 조회(현재시각기준)
+
+    @Query("SELECT coupon FROM Coupon coupon WHERE :cureentDateTime BETWEEN coupon.issuedAt AND coupon.expiredAt")
+    List<Coupon> findActiveCoupons(@Param("currentDateTime") LocalDateTime currentDateTime); // 발급일자 ~ 만료일자 사이인 모든 쿠폰들 조회
+
+    List<Coupon> findByCouponPolicy(CouponPolicy couponPolicy); // 쿠폰정책으로 쿠폰들 조회
+
+    Page<Coupon> findByStatus(Status status, Pageable pageable); // 특정상태인 쿠폰들 페이징 조회
 }
