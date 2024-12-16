1744cc59bfaa1678ebd83b8f634b32fcc1ff617b
thr13 <skhrnt2945@gmail.com>
2024-12-16 13:40:39 +0900

    MemberCouponRepository 추가

---
+++ src/main/java/com/nhnacademy/boostorenginx/repository/MemberCouponRepository.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.boostorenginx.repository;
+
+import com.nhnacademy.boostorenginx.entity.MemberCoupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long> {
+}
