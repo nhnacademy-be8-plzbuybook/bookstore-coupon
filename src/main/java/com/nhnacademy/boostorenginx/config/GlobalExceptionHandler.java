74238055a9da8be010a6c02c9a8cd94133150a37
thr13 <skhrnt2945@gmail.com>
2024-12-17 20:55:33 +0900

    GlobalExceptionHandler 수정

--- src/main/java/com/nhnacademy/boostorenginx/config/GlobalExceptionHandler.java
+++ src/main/java/com/nhnacademy/boostorenginx/config/GlobalExceptionHandler.java
@@ -11,32 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // NotFoundCouponPolicyException 처리 -> 404 에러
-    @ExceptionHandler(NotFoundCouponPolicyException.class)
-    public ResponseEntity<String> handleNotFoundCouponPolicyException(NotFoundCouponPolicyException ex) {
+    // 404 NOT FOUND
+    @ExceptionHandler({NotFoundCouponException.class, NotFoundCouponPolicyException.class, NotFoundMemberCouponException.class, NotFoundCouponHistoryException.class, NotFoundCouponTargetException.class})
+    public ResponseEntity<String> handleNotFoundException(RuntimeException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
-    // NotFoundMemberCouponException 처리 -> 404 에러
-    @ExceptionHandler(NotFoundMemberCouponException.class)
-    public ResponseEntity<String> handleNotFoundMemberCouponException(NotFoundMemberCouponException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
-
-    // NotFoundCouponException 처리 -> 404 에러
-    @ExceptionHandler(NotFoundCouponException.class)
-    public ResponseEntity<String> handleNotFoundCouponException(NotFoundCouponException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
-
-    // CouponHistory 예외 처리
-    @ExceptionHandler(NotFoundCouponHistoryException.class)
-    public ResponseEntity<String> handleNotFoundCouponHistoryException(NotFoundCouponHistoryException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
-
-    @ExceptionHandler(CouponHistoryException.class)
-    public ResponseEntity<String> handleCouponHistoryException(CouponHistoryException ex) {
+    // 400 BAD REQUEST
+    @ExceptionHandler({CouponException.class, CouponPolicyException.class, MemberCouponException.class, CouponHistoryException.class, CouponTargetException.class})
+    public ResponseEntity<String> handleCouponHistoryException(RuntimeException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
