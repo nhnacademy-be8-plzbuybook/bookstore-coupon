f81d390a152060b10170921eb5a9f0d81348c2c1
thr13 <skhrnt2945@gmail.com>
2024-12-19 14:46:40 +0900

    설정을 위해 전부 주석처리

--- src/main/java/com/nhnacademy/boostorenginx/controller/CouponController.java
+++ src/main/java/com/nhnacademy/boostorenginx/controller/CouponController.java
@@ -1,21 +1,21 @@
-package com.nhnacademy.boostorenginx.controller;
-
-import com.nhnacademy.boostorenginx.dto.coupon.CouponCreateRequestDto;
-import com.nhnacademy.boostorenginx.service.CouponService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/api/coupons")
-public class CouponController {
-    private final CouponService couponService;
-
-    @PostMapping
-    public ResponseEntity<?> registerCoupon(@RequestBody CouponCreateRequestDto dto) {
-        Long couponId = couponService.createCoupon(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(couponId);
-    }
-}
+//package com.nhnacademy.boostorenginx.controller;
+//
+//import com.nhnacademy.boostorenginx.dto.coupon.CouponCreateRequestDto;
+//import com.nhnacademy.boostorenginx.service.CouponService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("/api/coupons")
+//public class CouponController {
+//    private final CouponService couponService;
+//
+//    @PostMapping
+//    public ResponseEntity<?> registerCoupon(@RequestBody CouponCreateRequestDto dto) {
+//        Long couponId = couponService.createCoupon(dto);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(couponId);
+//    }
+//}
--- src/main/java/com/nhnacademy/boostorenginx/service/impl/CouponTargetServiceImpl.java
+++ src/main/java/com/nhnacademy/boostorenginx/service/impl/CouponTargetServiceImpl.java
@@ -1,38 +1,38 @@
-package com.nhnacademy.boostorenginx.service.impl;
-
-import com.nhnacademy.boostorenginx.entity.CouponPolicy;
-import com.nhnacademy.boostorenginx.entity.CouponTarget;
-import com.nhnacademy.boostorenginx.error.CouponTargetException;
-import com.nhnacademy.boostorenginx.error.NotFoundCouponPolicyException;
-import com.nhnacademy.boostorenginx.repository.CouponPolicyRepository;
-import com.nhnacademy.boostorenginx.repository.CouponTargetRepository;
-import com.nhnacademy.boostorenginx.service.CouponTargetService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-
-@RequiredArgsConstructor
-@Service
-public class CouponTargetServiceImpl implements CouponTargetService {
-
-    private final CouponTargetRepository couponTargetRepository;
-    private final CouponPolicyRepository couponPolicyRepository;
-
-    @Override
-    public void createCouponTarget(Long policyId, Long targetId) {
-        CouponPolicy couponPolicy = couponPolicyRepository.findById(policyId).orElseThrow(
-                        () -> new NotFoundCouponPolicyException("존재하지 않은 쿠폰 정책입니다: " + policyId)
-        );
-
-        if (couponTargetRepository.findByTargetId(targetId).isPresent()) {
-            throw new CouponTargetException("이미 등록된 쿠폰 대상 입니다");
-        }
-
-        CouponTarget couponTarget = CouponTarget.builder()
-                .couponPolicy(couponPolicy)
-                .targetId(targetId)
-                .build();
-
-        couponTargetRepository.save(couponTarget);
-    }
-}
+//package com.nhnacademy.boostorenginx.service.impl;
+//
+//import com.nhnacademy.boostorenginx.entity.CouponPolicy;
+//import com.nhnacademy.boostorenginx.entity.CouponTarget;
+//import com.nhnacademy.boostorenginx.error.CouponTargetException;
+//import com.nhnacademy.boostorenginx.error.NotFoundCouponPolicyException;
+//import com.nhnacademy.boostorenginx.repository.CouponPolicyRepository;
+//import com.nhnacademy.boostorenginx.repository.CouponTargetRepository;
+//import com.nhnacademy.boostorenginx.service.CouponTargetService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//
+//@RequiredArgsConstructor
+//@Service
+//public class CouponTargetServiceImpl implements CouponTargetService {
+//
+//    private final CouponTargetRepository couponTargetRepository;
+//    private final CouponPolicyRepository couponPolicyRepository;
+//
+//    @Override
+//    public void createCouponTarget(Long policyId, Long targetId) {
+//        CouponPolicy couponPolicy = couponPolicyRepository.findById(policyId).orElseThrow(
+//                        () -> new NotFoundCouponPolicyException("존재하지 않은 쿠폰 정책입니다: " + policyId)
+//        );
+//
+//        if (couponTargetRepository.findByTargetId(targetId).isPresent()) {
+//            throw new CouponTargetException("이미 등록된 쿠폰 대상 입니다");
+//        }
+//
+//        CouponTarget couponTarget = CouponTarget.builder()
+//                .couponPolicy(couponPolicy)
+//                .targetId(targetId)
+//                .build();
+//
+//        couponTargetRepository.save(couponTarget);
+//    }
+//}
