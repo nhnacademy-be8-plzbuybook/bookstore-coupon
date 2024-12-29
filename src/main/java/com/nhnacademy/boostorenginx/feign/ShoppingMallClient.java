a640dd48c1b7b46a2cfcbf2c532822deb986e20a
sugwan <skhrnt2945@gmail.com>
2024-12-29 15:21:41 +0900

    카테고리 쿠폰 설정

--- src/main/java/com/nhnacademy/boostorenginx/feign/ShoppingMallClient.java
+++ src/main/java/com/nhnacademy/boostorenginx/feign/ShoppingMallClient.java
@@ -1,9 +1,18 @@
-//package com.nhnacademy.boostorenginx.feign;
-//
-//import org.springframework.cloud.openfeign.FeignClient;
-//
-//@FeignClient(name = "bookstore")
-//public interface ShoppingMallClient {
-//
-//}
-//
+package com.nhnacademy.boostorenginx.feign;
+
+import com.nhnacademy.boostorenginx.dto.category.CategorySimpleResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "bookstore")
+public interface ShoppingMallClient {
+
+    // 쇼핑몰 서버에 카테고리 정보요청
+    @GetMapping("/api/categories/search")
+    List<CategorySimpleResponseDto> searchCategories(@RequestParam String keyword);
+}
+
