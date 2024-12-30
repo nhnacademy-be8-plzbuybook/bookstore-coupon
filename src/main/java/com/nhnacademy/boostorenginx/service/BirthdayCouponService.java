98cb4ef65649d18311a9aace566fa39e09b8029f
sugwan <skhrnt2945@gmail.com>
2024-12-30 11:25:40 +0900

    생일쿠폰 발급 수정

--- src/main/java/com/nhnacademy/boostorenginx/service/BirthdayCouponService.java
+++ src/main/java/com/nhnacademy/boostorenginx/service/BirthdayCouponService.java
@@ -7,16 +7,18 @@ import com.nhnacademy.boostorenginx.dto.coupon.CouponResponseDto;
 import com.nhnacademy.boostorenginx.dto.couponpolicy.CouponPolicyResponseDto;
 import com.nhnacademy.boostorenginx.dto.couponpolicy.CouponPolicySaveRequestDto;
 import com.nhnacademy.boostorenginx.dto.coupontarget.CouponTargetAddRequestDto;
+import com.nhnacademy.boostorenginx.dto.coupontarget.CouponTargetResponseDto;
 import com.nhnacademy.boostorenginx.dto.membercoupon.MemberCouponCreateRequestDto;
 import com.nhnacademy.boostorenginx.enums.SaleType;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class BirthdayCouponService {
@@ -45,25 +47,31 @@ public class BirthdayCouponService {
 
         // Birthday 쿠폰정책 생성
         CouponPolicyResponseDto couponPolicyResponseDto = couponPolicyService.createCouponPolicy(couponPolicySaveRequestDto);
+        Long couponPolicyId = couponPolicyResponseDto.id();
+        log.debug("CouponPolicyId: {}, MemberId: {}", couponPolicyId, requestDto.memberId());
 
         // Birthday 쿠폰대상 생성
         CouponTargetAddRequestDto couponTargetAddRequestDto = new CouponTargetAddRequestDto(
-                couponPolicyResponseDto.id(),
+                couponPolicyId,
                 requestDto.memberId()
         );
-        couponTargetService.createCouponTarget(couponTargetAddRequestDto);
+        CouponTargetResponseDto couponTargetResponseDto = couponTargetService.createCouponTarget(couponTargetAddRequestDto);
+        Long couponTargetId = couponTargetResponseDto.id();
+        log.debug("CouponTargetId: {}", couponTargetId);
 
         // Birthday 쿠폰 생성
         CouponCreateRequestDto couponCreateRequestDto = new CouponCreateRequestDto(
-                couponPolicyResponseDto.id(),
+                couponPolicyId,
                 requestDto.registerAt()
         );
         CouponResponseDto couponResponseDto = couponService.createCoupon(couponCreateRequestDto);
+        Long couponId = couponResponseDto.id();
+        log.debug("CouponId: {}", couponId);
 
         // 회원쿠폰 발급
         MemberCouponCreateRequestDto memberCouponCreateRequestDto = new MemberCouponCreateRequestDto(
                 requestDto.memberId(),
-                couponResponseDto.id(),
+                couponId,
                 0,
                 10
         );
