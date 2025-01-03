3496c68a2bdd251195a89ba4ea6df00861150916
sugwan <skhrnt2945@gmail.com>
2025-01-03 16:39:03 +0900

    chore: 주문상품에 쿠폰할인을 적용하는 기능 추가 (CouponCalculationController)

--- src/main/java/com/nhnacademy/boostorenginx/service/CouponCalculationService.java
+++ src/main/java/com/nhnacademy/boostorenginx/service/CouponCalculationService.java
@@ -5,6 +5,6 @@ import com.nhnacademy.boostorenginx.dto.calculation.CouponCalculationResponseDto
 
 public interface CouponCalculationService {
 
-    CouponCalculationResponseDto applyOrderProductCoupon(CouponCalculationRequestDto couponCalculationRequestDto); // 회원이 쿠폰을 사용할때 가격 계산
+    CouponCalculationResponseDto applyOrderProductCoupon(Long memberId, Long couponId, CouponCalculationRequestDto couponCalculationRequestDto); // 회원이 쿠폰을 사용할때 가격 계산
 
 }
