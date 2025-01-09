642ffe35e0b03524ce6ce688df9887954cae7b8e
sugwan <skhrnt2945@gmail.com>
2025-01-09 15:55:41 +0900

    fix: 책/카테고리 검색 쿠폰 관련 정보 제거
    프론트 페이지에서 각각 호출하도록 변경

--- src/main/java/com/nhnacademy/boostorenginx/service/SellingBookCouponService.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.boostorenginx.service;
-
-import com.nhnacademy.boostorenginx.dto.couponpolicy.CouponPolicySaveRequestDto;
-
-public interface SellingBookCouponService {
-
-    void createCouponForSellingBook(String searchKeyword, CouponPolicySaveRequestDto couponPolicySaveRequestDto); // 도서쿠폰 발급
-
-}
